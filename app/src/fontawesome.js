0356313cd7b4847cc690c88ec9c69300eafc7d17
K.V.Jaswanth <kvjaswanth09@gmail.com>
2025-06-21 19:54:44 +0530

    Delete app directory

--- app/src/fontawesome.js
+++
@@ -1,5 +0,0 @@
-// fontawesome.js
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEye, faPlus, faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faEye, faPlus, faTrash, faEdit);
